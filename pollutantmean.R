893a477fc567e818d560fb60125441883dd5ba7d
nanomuthu-R <bimuthukumar@yahoo.com>
2015-05-14 16:55:53 +0530

    proj. assigment 1 pollutantmean.R completed.

---
+++ pollutantmean.R
@@ -0,0 +1,25 @@
+pollutantmean <- function(directory, pollutant, id=1:322){
+  
+  file_list <- list.files(directory, full.names=TRUE)
+  dat <- data.frame()
+  
+          for (i in id)
+          {
+            dat <- rbind(dat, read.csv(file_list[i]))
+          }
+    
+         if (names(dat["sulfate"])==pollutant)
+           
+         {
+           m_s <- mean(dat$sulfate, na.rm=TRUE)
+           print(m_s)
+         }  
+         
+         if (names(dat["nitrate"])==pollutant)
+           
+         { 
+           m_n <- mean(dat$nitra, na.rm=TRUE)
+           print(m_n)
+         
+          }
+}
